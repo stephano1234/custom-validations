6a396f3aeac20726154c5fc0cfa71e1acaaa2ff4
Main <Main@DESKTOP-EIQ564P>
2020-01-26 19:27:43 -0300

    c

--- custom-validation/src/main/java/br/com/contmatic/validacoes/utilidades/ConstantesString.java
+++ custom-validation/src/main/java/br/com/contmatic/validacoes/utilidades/ConstantesString.java
@@ -18,10 +18,7 @@ public class ConstantesString {
     public static final String EMAIL = "^[a-z0-9][a-z0-9._-]{0,29}(?<![._-])@[a-z0-9][a-z0-9.-]{0,19}(?<![.-])\\.[a-z]{2,6}$";    
     
     /** The Constant NOME. */
-    public static final String NOME = "[A-ZÁÉÍÓÚÃÕÀÂÊÔÇ]{1," + (ConstantesNumericas.TAMANHO_REGULAR) + "}";
-    
-    /** The Constant NOT_BLANK. */
-    public static final String NOT_BLANK = ".*\\S.*";
+    public static final String NOME = "[A-ZÁÉÍÓÚÃÕÀÂÊÔÇ ]{1," + (ConstantesNumericas.TAMANHO_REGULAR) + "}";
     
     /** The Constant CELULAR. */
     public static final String CELULAR = NUMERAL + "{" + ConstantesNumericas.NUMERO_CELULAR + "}";
@@ -48,7 +45,7 @@ public class ConstantesString {
     public static final String NUMERO_CONTA = APENAS_LETRA_NUMERAL + "{1," + ConstantesNumericas.MAX_NUMERO_CONTA + "}";
     
     /** The Constant RAZAO_SOCIAL. */
-    public static final String RAZAO_SOCIAL = "([A-ZÁÉÍÓÚÃÕÀÂÊÔÇ]|[:\\)\\(&'\\.-]){1," + (ConstantesNumericas.TAMANHO_REGULAR) + "}";
+    public static final String RAZAO_SOCIAL = "([A-ZÁÉÍÓÚÃÕÀÂÊÔÇ ]|[:\\)\\(&'\\.-]){1," + (ConstantesNumericas.TAMANHO_REGULAR) + "}";
     
     /**
      * Instantiates a new constantes string.
